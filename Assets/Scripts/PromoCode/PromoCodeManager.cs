62fc6e4bfbccd26bccb290f51c6699e30d90c436
Mrrebrik <itibsoft@gmail.com>
2022-02-06 01:33:46 +0300

    - Base system PromoCode

---
+++ Assets/Scripts/PromoCode/PromoCodeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PromoCodeManager : SingletonMono<PromoCodeManager>
+{
+	public const string Love10 = "Love10";
+	public const string Love100 = "Love100";
+	public const string Love1000 = "Love1000";
+
+	public void PromoCodoActivate(string code)
+	{
+		switch (code)
+		{
+			case Love10:
+				Debug.Log("10");
+				break;
+			case Love100:
+				Debug.Log("100");
+				break;
+			case Love1000:
+				Debug.Log("1000");
+				break;
+		}
+	}
+}
---
+++ Assets/Scripts/PromoCode/PromoHolder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PromoHolder : MonoBehaviour
+{
+	[SerializeField] private TMP_InputField _input;
+
+	public void EnterCode()
+	{
+		PromoCodeManager.Instance.PromoCodoActivate(_input.text);
+		_input.text = null;
+	}
+
+}
