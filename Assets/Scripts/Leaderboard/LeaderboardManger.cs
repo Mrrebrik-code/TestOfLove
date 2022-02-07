513cc83d3950f2fe01e516c68b5c38ae85cf031e
Mrrebrik <itibsoft@gmail.com>
2022-02-07 18:22:46 +0300

    - Base class to leaderboard

---
+++ Assets/Scripts/Leaderboard/LeaderboardManger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderboardManger : SingletonMono<LeaderboardManger>
+{
+	private const string _nameLeaderboard = "name";
+	public void SetLeaderboardScore(int score, string description)
+	{
+
+	}
+
+	public void GetLeaderboardScorePlayerEntry()
+	{
+
+	}
+}
