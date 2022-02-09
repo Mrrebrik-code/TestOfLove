0b7b0ae1d54a28fddc0de142f33e821cb82c01b6
Mrrebrik <itibsoft@gmail.com>
2022-02-10 01:59:44 +0300

    - Base system AudioManager [Sounds]

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : SingletonMono<AudioManager>
+{
+	[SerializeField] private AudioSource _soundSource;
+	[SerializeField] private AudioSource _musicSource;
+	[SerializeField] private AudioSource _sfxSource;
+
+	[SerializeField] private List<Audio> _soundsAudio = new List<Audio>();
+	private Dictionary<Sounds, AudioClip> _clipSounds = new Dictionary<Sounds,AudioClip>();
+
+	public override void Awake()
+	{
+		base.Awake();
+		Init();
+	}
+
+	private void Init()
+	{
+		_soundsAudio.ForEach(audio =>
+		{
+			_clipSounds.Add(audio.Type, audio.Clip);
+		});
+	}
+
+	public void PlaySound(Sounds type)
+	{
+		var clip = _clipSounds[type];
+		_soundSource.PlayOneShot(clip);
+	}
+
+	[System.Serializable]
+	public class Audio
+	{
+		[SerializeField] private Sounds _type;
+		[SerializeField] private AudioClip _clip;
+
+		public Sounds Type { get { return _type; } }
+		public AudioClip Clip { get { return _clip;} }
+	}
+}
